d0317389016c8f1f986632a380c426ce8cd12524
Atakan Bayrak <atakanbayrak461@hotmail.com>
2021-12-07 22:56:44 +0100

    7Aralık

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,14 +5,32 @@ class Counter extends React.Component {
     count: 0,
   };
 
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+  handleIncrement() {
+    console.log("Increment Clicked", this);
+  }
+
   render() {
     return (
-      <React.Fragment>
-        <span>{this.formatCount()}</span>
-
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
